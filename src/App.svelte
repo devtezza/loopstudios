47b15ec0e6a83954c74b0043180abd9d820aaf63
nandotezza <fernando.tezza@gmail.com>
2022-05-23 20:36:12 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import './styles/main.css'
+  import Footer from './lib/Footer.svelte';
+</script>
+
+<main>
+  
+  About
+  Careers
+  Events
+  Products
+  Support
+
+  <h1>Immersive experiences that deliver</h1>
+
+  
+
+  The leader in interactive VR
+  <Footer />
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <div class="copyright">
+        © 2021 Loopstudios. All rights reserved.
+    </div>
+    
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://www.devtezza.com/">Fernando Tezza</a>.
+  </div>
+</footer>
+
+<style>
+    .copyright {
+        text-align: center;
+    }
+    .attribution { font-size: 11px; text-align: center; }
+    .attribution a { color: hsl(228, 45%, 44%); }
+</style>
