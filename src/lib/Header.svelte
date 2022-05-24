b872bfb88674f282cd01b5c50c9985f8ddf1e939
nandotezza <fernando.tezza@gmail.com>
2022-05-23 22:12:43 -0300

    Create Header, Home, Logo and Navbar components

---
+++ src/lib/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Logo from "./Logo.svelte";
+    import Navbar from "./Navbar.svelte";
+</script>
+<header class="header">
+    <Logo />
+    <Navbar />
+</header>
+<style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,25 @@
+<nav class="navbar">
+    <ul class="navbar-menu">
+        <li><a href="">About</a></li>
+        <li><a href="">Careers</a></li>
+        <li><a href="">Events</a></li>
+        <li><a href="">Products</a></li>
+        <li><a href="">Support</a></li>      
+    </ul>
+</nav>
+
+<style>
+   ul {
+       display: flex;
+       justify-content: space-between;
+   }
+
+   li {
+       margin-left: 1rem;
+   }
+</style>
+
+
+  
+  
+  
