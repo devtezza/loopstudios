0a94337517357a4f8fa202a030584f9be498b854
nandotezza <fernando.tezza@gmail.com>
2022-05-24 21:44:54 -0300

    Create Close component

---
+++ src/lib/Close.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let isOpen;
+    import { fade } from 'svelte/transition';
+</script>
+<div class="close" class:transition={isOpen}>
+    <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#FFF" fill-rule="evenodd"/></svg>
+</div>
+<style>
+   
+    .close.transition {
+        /* border: 1px solid red; */
+        
+        animation: var(--animation-fade-in);
+        animation-duration: 500ms;
+    }   
+</style>
