3be5bc1edc3163ba9808fbd8ce3160032e02418b
nandotezza <fernando.tezza@gmail.com>
2022-05-28 12:13:12 -0300

    Finish mobile layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
-  import './styles/main.css'
+  import './styles/main.css';
+  import './styles/fonts.css';
   import Footer from './lib/Footer.svelte';
   import Home from './lib/Home.svelte';
+  import Content from './lib/Content.svelte';
 </script>
 
-<main>
-  
+<main>  
   <Home />
+  <Content />
   <Footer />
 </main>
 
